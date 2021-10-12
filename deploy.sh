aa836bd3cea036c2167e5b08429755b447ea0e75
georgi_lazarov <grg.lazarov@gmail.com>
2021-10-12 15:10:21 +0200

    webpage updates

--- deploy.sh
+++ deploy.sh
@@ -2,16 +2,29 @@
 
 set -e
 
-#git checkout --orphan gh-pages # stores a repo's website inside the repo but on a separate branch, keeps only the website's history on this branch.
-npm run build
-git add --all
-git commit -m "changes"
-cd dist
-git checkout gh-pages
-git add . # adds everything only in the dist folder
-git commit -m "new deployment"
-git push  origin gh-pages
+echo *** commiting work on master branch ***
+git add .
+git commit -m "webpage updates"
 
-cd -
-git checkout master
+echo *** checking out GitHub Pages branch ***
+
+git checkout --orphan gh-pages # stores a repo's website inside the repo but on a separate branch, keeps only the website's history on this branch.
+
+echo *** ... Running new build ***
+
+npm run build
+
+git --work-tree dist add --all # adds everything only in the dist folder
+
+echo *** ... Pushing to remote gh-pages branch ***
+
+git --work-tree dist commit -m "new deployment"
+git push  origin HEAD:gh-pages --force
+
+rm -r dist
+git branch -D gh-pages
+
+echo *** checking out Master branch ***
+
+git checkout -f master
 
